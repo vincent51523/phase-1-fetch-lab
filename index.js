9cb79848f0d36b4664cfdcf3eae94f5a99ca67aa
bin <vincent418@icloud.com>
2021-05-19 16:46:55 -0500

    completed assigment

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-function fetchBooks() {
+function fetchBooks () {
   return fetch('https://anapioficeandfire.com/api/books')
-  .then(resp => resp(json))
-  .then(json => rendersBooks(json))
+  .then(resp => resp.json())
+  .then(json => renderBooks(json))
 }
 
-function renderBooks(books) {
+
+
+
+ function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
