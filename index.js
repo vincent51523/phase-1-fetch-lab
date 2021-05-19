755a236ff3037d64835c84e2cb69eedc804c15c2
bin <vincent418@icloud.com>
2021-05-19 16:36:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp(json))
+  .then(json => rendersBooks(json))
 }
 
 function renderBooks(books) {
